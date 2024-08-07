0b19b2af4bdd040fcc9a0de42433c56b3f281122
Kiprotich marion <marionjemutai71@gmail.com>
2024-08-08 01:03:07 +0300

    Create ComponentA.jsx

---
+++ context-project/src/ComponentA.jsx
@@ -0,0 +1,17 @@
+import React, {useState, createContext} from 'react';
+import ComponentB from './ComponentB';
+export const userContext = createContext();
+function ComponentA(){
+    const [user, setUser] = useState("Kiprotich")
+return(
+    <div className="componet">
+        <h1>ComponentA</h1>
+        <h2>{`Hello ${user}`}</h2>
+        <userContext.Provider value = {user}>
+       <ComponentB user= {user}/>
+       </userContext.Provider>
+
+    </div>
+);
+}
+export default ComponentA;
