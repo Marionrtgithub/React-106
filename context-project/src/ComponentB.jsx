95e459a21e066c6e3cecad2cbca497f36e9e1575
Kiprotich marion <marionjemutai71@gmail.com>
2024-08-08 00:56:54 +0300

    Create ComponentB.jsx

---
+++ context-project/src/ComponentB.jsx
@@ -0,0 +1,10 @@
+import ComponentC from './ComponentC';
+function ComponentB(){
+return(
+    <div className="componet">
+        <h1>ComponentB</h1>
+        <ComponentC />
+    </div>
+);
+}
+export default ComponentB;
